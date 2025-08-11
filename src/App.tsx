12d99fb7a46e8d8006f98baf300bf8402993b069
Nimuel <yiduolnemya@gmail.com>
2025-08-11 11:21:49 -0400

    feat: setup react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Outlet } from "react-router";
 import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <div className="flex justify-center items-stretch grow">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./pages/Home.tsx";
+import Result from "./pages/Result.tsx";
+import Test from "./pages/Test.tsx";
+import Cities from "./pages/Cities.tsx";
+import About from "./pages/About.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <App />
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/cities" element={<Cities />} />
+          <Route path="/test" element={<Test />} />
+          <Route path="/result" element={<Result />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
