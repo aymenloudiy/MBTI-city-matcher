e05160bba663686d0fa836e51a0e2b2e39033adb
Nimuel <yiduolnemya@gmail.com>
2025-08-13 18:09:40 -0400

    style: fix navabr css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,13 +4,13 @@ function Navbar() {
   return (
     <nav className="mx-16">
       <ul className="flex justify-between gap-8">
-        <li className="my-4">
+        <li className="py-4 px-4 bg-white rounded-xl">
           <NavLink to="/test">Take the test</NavLink>
         </li>
-        <li className="my-4">
+        <li className="py-4 px-4 bg-white rounded-xl">
           <NavLink to="/cities">Cities</NavLink>
         </li>
-        <li className="my-4">
+        <li className="py-4 px-4 bg-white rounded-xl">
           <NavLink to="/about">About Us</NavLink>
         </li>
       </ul>
