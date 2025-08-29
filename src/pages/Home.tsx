cafd646775309810e557bd5b67f11c352c8ca275
Seongyeon Chang <chan0596@algonquinlive.com>
2025-08-29 16:18:41 -0400

    change main banner padding and margin size

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,10 +3,11 @@ import ReviewSection from "../components/ReviewSection";
 
 function Home() {
   return (
-    <div className="flex flex-col grow mx-16 justify-center items-center">
+    <div className="flex flex-col grow justify-center items-center max-w-5xl mx-auto px-6 md:px-16 space-y-12">
       <MainBanner />
       <ReviewSection />
     </div>
   );
 }
+
 export default Home;
