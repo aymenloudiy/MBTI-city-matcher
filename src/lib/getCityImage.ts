279514352c291e9549f573b14a7ae87f342cc4ed
Seongyeon Chang <chan0596@algonquinlive.com>
2025-10-12 09:55:23 -0400

    change the getCityimage

--- src/lib/getCityImage.ts
+++ src/lib/getCityImage.ts
@@ -1,7 +1,4 @@
 export function getCityImage(cityName: string): string {
-  const formattedName = cityName
-    .toLowerCase()
-    .replace(/\s+/g, "")
-    .replace(/[.'"]/g, "");
+  const formattedName = cityName.replace(/\s+/g, "").replace(/[.'"]/g, "");
   return `/cities/${formattedName}.jpg`;
 }
